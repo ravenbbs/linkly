22c33ab15452dcea5ae010af53f0d3358dcfce17
Julio Condor <julioc3bgud@gmail.com>
2024-01-23 23:44:40 -0500

    Body Style css - Header component on Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Header from "@/components/Header";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h2>Test</h2>
-    </div>
+    <main>
+      <Header />
+      <section></section>
+    </main>
   );
 }
