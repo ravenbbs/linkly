8cefe014c658f77ab42ea2871eb0c8b25dd16e51
Julio Condor <julioc3bgud@gmail.com>
2024-01-24 00:38:47 -0500

    Header component style

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,20 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="font-semibold bg-white border-b flex justify-between p-4">
-      <div className="flex gap-4">
-        <Link href={"/"}>LinkList</Link>
-
-        <nav className="flex items-center gap-2 text-slate-500 text-sm">
-          <Link href={"/about"}>Acerca de</Link>
-          <Link href={"/pricing"}>Precios</Link>
-          <Link href={"/contact"}>Contacto</Link>
-        </nav>
-      </div>
-      <div className="flex gap-4 items-center text-slate-500 text-sm">
-        <Link href={"/login"}>Ingresa</Link>
-        <Link href={"/register"}>Regístrate</Link>
+    <header className="font-semibold bg-white border-b  py-4">
+      <div className="max-w-4xl flex justify-between mx-auto px-4">
+        <div className="flex gap-4">
+          <Link href={"/"}>LinkList</Link>
+          <nav className="flex items-center gap-2 text-slate-500 text-sm">
+            <Link href={"/about"}>Acerca de</Link>
+            <Link href={"/pricing"}>Precios</Link>
+            <Link href={"/contact"}>Contacto</Link>
+          </nav>
+        </div>
+        <div className="flex gap-4 items-center text-slate-500 text-sm">
+          <Link href={"/login"}>Ingresa</Link>
+          <Link href={"/register"}>Regístrate</Link>
+        </div>
       </div>
     </header>
   );
