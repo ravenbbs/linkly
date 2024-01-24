13df47a28adba284f02d6f06693c5e463d4e51f3
Julio Condor <julioc3bgud@gmail.com>
2024-01-24 00:15:14 -0500

    Home page - Details

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,24 @@ export default function Home() {
   return (
     <main>
       <Header />
-      <section></section>
+      <section className="font-semibold p-4">
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold">
+            Tu único link <br />
+            para Todo
+          </h1>
+          <h2 className="text-slate-500 text-xl mt-6">
+          Unifica y comparte tus enlaces, redes sociales,
+  información de contacto y más, todo en una página.
+          </h2>
+        </div>
+        <form className="flex flex-col gap-4">
+          <input type="text"/>
+          <button type="submit" className="rounded-lg bg-blue-500 text-white py-4 px-6" >
+            Unirse Gratis
+          </button>
+        </form>
+      </section>
     </main>
   );
 }
