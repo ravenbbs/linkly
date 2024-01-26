58390a5fbab717dfa2d56420c191eb30d1f869d5
Julio Condor <julioc3bgud@gmail.com>
2024-01-25 23:39:41 -0500

    Register Page

---
+++ src/app/register/page.js
@@ -0,0 +1,14 @@
+export default function Login() {
+  return (
+    <div>
+      <div className="bg-white border p-4 max-w-xs mx-auto mt-4">
+        <h1 className=" text-4xl font-bold text-center" >
+          Regístrate
+        </h1>
+        <button className="btn-primary text-white bg-blue-500">
+          Inicia Con Google
+        </button>
+      </div>
+    </div>
+  );
+}
