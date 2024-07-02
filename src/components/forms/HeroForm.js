60408fc587b92dfef21a92c43ec3a03cd32fd81a
ravenbbs <julioc3bgud@gmail.com>
2024-07-01 23:33:11 -0500

    Add functionality fir no session

--- src/components/forms/HeroForm.js
+++ src/components/forms/HeroForm.js
@@ -1,36 +1,42 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { redirect } from "next/navigation";
-import { useEffect } from "react";
+import {redirect, useRouter} from "next/navigation";
+import {useEffect} from "react";
 
-export default function HeroForm() {
-
+export default function HeroForm({user}) {
+  const router = useRouter();
   useEffect(() => {
-    if("localStorage" in window && window.localStorage.getItem("desiredUsername")) {
-      const userName = window.localStorage.getItem("desiredUsername")
-      window.localStorage.removeItem("desiredUsername")
-      redirect('/account?desiredUsername=' + userName)
+    if (
+      'localStorage' in window
+      && window.localStorage.getItem('desiredUsername')
+    ) {
+      const username = window.localStorage.getItem('desiredUsername');
+      window.localStorage.removeItem('desiredUsername');
+      redirect('/account?desiredUsername=' + username);
     }
-  }, [])
+  }, []);
 
   async function handleSubmit(ev) {
     ev.preventDefault();
-
-    const form = ev.target
-    const input = form.querySelector("input")
-    const userName = input.value
-
-    if(userName.length > 0) {
-      window.localStorage.setItem("desiredUsername", userName)
-      await signIn("google")
+    const form = ev.target;
+    const input = form.querySelector('input');
+    const username = input.value;
+    if (username.length > 0) {
+      if (user) {
+        router.push('/account?desiredUsername='+username);
+      } else {
+        window.localStorage.setItem('desiredUsername', username);
+        await signIn('google');
+      }
     }
   }
+
 
   return (
     <form
       onSubmit={handleSubmit}
-      className=" inline-flex items-center shadow-xl"
+      className=" inline-flex items-center shadow-xl text-white"
     >
       <span className="py-4 bg-secondary rounded-ss-lg rounded-es-lg pl-4 text-white">
         linkly.es/
@@ -38,7 +44,7 @@ export default function HeroForm() {
       <input
         type="text"
         placeholder="username"
-        className="py-4 bg-secondary placeholder-white/70 outline-none"
+        className="py-4 bg-secondary text-white placeholder-white/70 outline-none"
       />
       <button
         type="submit"
