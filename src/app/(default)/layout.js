bafec9663b167c68197f949e8642166eabecc105
ravenbbs <julioc3bgud@gmail.com>
2024-07-03 17:23:25 -0500

    refactor router groups (default)

--- src/app/(default)/layout.js
+++ src/app/(default)/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
+import { Toaster } from "react-hot-toast";
 import Header from "@/components/Header";
-import toast, { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +10,7 @@ export const metadata = {
   description:
     "Linkly te permite centralizar y compartir todos tus enlaces, redes sociales, información de contacto y más en una página única. Simplifica tu presencia en línea de manera elegante y eficiente.",
   referrer: "origin-when-cross-origin",
+  icons: "/favicon.ico",
   keywords: [
     "linkly",
     " enlaces",
@@ -25,9 +26,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es" data-theme="nord">
-      <body className={inter.className + " relative min-h-screen"}>
-        {/* blur bg */}
-        {/* <div className="absolute -z-20 inset-0 bg-base-300 bg-[size:20px_20px] opacity-20 blur-[100px]"></div> */}
+      <body
+        className={
+          inter.className +
+          " relative min-h-screen flex flex-col justify-between"
+        }
+      >
         {/* bg ibelik mallas */}
         <div className="absolute z-[-2] bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_top,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_30%,transparent_100%)]"></div>
         <div className="absolute z-[-2] bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_top,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_60%_at_50%_100%,#000_30%,transparent_100%)]"></div>
@@ -38,8 +42,8 @@ export default function RootLayout({ children }) {
           <Toaster />
         </main>
 
-        <footer className="footer bg-base-300/80 px-12 py-4 absolute bottom-2 left-1/2 transform -translate-x-1/2 max-w-4xl rounded-md">
-          <aside className="grid-flow-col items-center">
+        <footer className="footer sm:grid-flow-col bg-base-300/80 px-12 py-2 gap-2 sm:py-4 max-w-4xl rounded-md self-center mt-12 mb-3 shadow-xl ">
+          <aside className="grid-flow-col font-medium mx-auto sm:mx-2">
             <p>
               Designed by{" "}
               <a
@@ -51,43 +55,11 @@ export default function RootLayout({ children }) {
               </a>
             </p>
           </aside>
-          <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
+          <nav className="grid-flow-col gap-4 mx-auto sm:mx-0 sm:ml-auto">
             <a
-              href="#"
-              className="text-primary transition-all hover:text-primary/80 "
+              href="https://www.linkedin.com/in/julio-condor/"
+              className="text-primary transition-all hover:text-primary/80"
+              target="_blank"
             >
               <svg
                 className="w-6 h-6"
@@ -101,8 +73,9 @@ export default function RootLayout({ children }) {
             </a>
 
             <a
-              href="#"
-              className="text-primary hover:text-base-content transition-all ms-5"
+              href="https://github.com/ravenbbs"
+              className="text-primary hover:text-base-content transition-all "
+              target="_blank"
             >
               <svg
                 className="w-6 h-6"
