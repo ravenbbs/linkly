e03a68e42559d4d69391b3437c5027457e0dd0f6
ravenbbs <julioc3bgud@gmail.com>
2024-07-23 21:28:22 -0500

    add page for app

--- src/app/(app)/account/page.js
+++ src/app/(app)/account/page.js
@@ -4,7 +4,11 @@ import { redirect } from "next/navigation";
 import { Page } from "@/models/Page";
 import mongoose from "mongoose";
 import PageSettingsForm from "@/components/forms/PageSettingForm";
+import PageButtonsForm from "@/components/forms/PageButtonsForm";
 import UsernameForm from "@/components/UsernameForm";
+import PageLinksForm from "@/components/forms/PageLinkForm";
+import { User } from "@/models/User";
+
 export default async function AccountPage({ searchParams }) {
   const session = await getServerSession(authOptions);
   const desiredUsername = searchParams?.desiredUsername;
@@ -14,14 +18,17 @@ export default async function AccountPage({ searchParams }) {
   }
   mongoose.connect(process.env.MONGODB_URI);
 
-  const page = await Page.findOne({owner: session?.user?.email})
+  const page = await Page.findOne({ owner: session?.user?.email});
+  const user = await User.findOne({owner: session?.user?.email})
 
   if (page) {
-    return (<>
-    <PageSettingsForm/>
-    </>
-    
-  )
+    return (
+      <div className="flex flex-col gap-2 max-w-4xl self-end bg-base-300 mt-24 sm:mt-0">
+        <PageSettingsForm page={page} user={user} session={session} />
+        <PageButtonsForm page={page} />
+        <PageLinksForm page={page} user={session?.user} />
+      </div>
+    );
   }
 
   return (
