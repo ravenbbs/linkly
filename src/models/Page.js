ab95a5a8fa657f8fc54e9809b153254b0266ce06
ravenbbs <julioc3bgud@gmail.com>
2024-07-25 23:04:06 -0500

    edit model Pagr

--- src/models/Page.js
+++ src/models/Page.js
@@ -9,7 +9,15 @@ const pageSchema = new Schema({
     min: 1,             // El valor mínimo de longitud para 'uri' es 1
     unique: true        // El valor de 'uri' debe ser único en la colección
   },
-  owner: {type: String , required: true}
+  owner: {type: String , required: true},
+  displayName: {type: String, default: ''},
+  location: {type: String, default: ''},
+  bio: {type: String, default: ''},
+  bgType: {type: String, default: 'color'},
+  bgColor: {type: String, default: '#00cfbd'},
+  bgImage: {type: String, default: ''},
+  buttons: {type: Object, default: {}},
+  links: {type: Object, default: []},
 }, { 
   timestamps: true      // Agrega automáticamente los campos 'createdAt' y 'updatedAt'
 });
