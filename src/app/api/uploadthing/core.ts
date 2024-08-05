2e7d42f7139b7966b3b2db0d39f698d1c86d6ac9
ravenbbs <julioc3bgud@gmail.com>
2024-08-05 10:12:43 -0500

    uploadthing button

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,10 +1,10 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
- 
+
 const f = createUploadthing();
- 
+
 const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
- 
+
 // FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
@@ -13,10 +13,10 @@ export const ourFileRouter = {
     .middleware(async ({ req }) => {
       // This code runs on your server before upload
       const user = await auth(req);
- 
+
       // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError("Unauthorized");
- 
+
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.id };
     })
@@ -28,5 +28,5 @@ export const ourFileRouter = {
       return { uploadedBy: metadata.userId };
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
--- src/models/Page.js
+++ src/models/Page.js
@@ -2,25 +2,28 @@
 const { Schema, model, models } = require("mongoose");
 
 // Definimos el esquema para la colección 'pages'
-const pageSchema = new Schema({
-  uri: { 
-    type: String,       // El tipo de dato del campo 'uri' es String
-    required: true,     // El campo 'uri' es obligatorio
-    min: 1,             // El valor mínimo de longitud para 'uri' es 1
-    unique: true        // El valor de 'uri' debe ser único en la colección
+const pageSchema = new Schema(
+  {
+    uri: {
+      type: String, // El tipo de dato del campo 'uri' es String
+      required: true, // El campo 'uri' es obligatorio
+      min: 1, // El valor mínimo de longitud para 'uri' es 1
+      unique: true, // El valor de 'uri' debe ser único en la colección
+    },
+    owner: { type: String, required: true },
+    displayName: { type: String, default: "" },
+    location: { type: String, default: "" },
+    bio: { type: String, default: "" },
+    bgType: { type: String, default: "color" },
+    bgColor: { type: String, default: "#00cfbd" },
+    bgImage: { type: String, default: "" },
+    buttons: { type: Object, default: {} },
+    links: { type: Object, default: [] },
   },
-  owner: {type: String , required: true},
-  displayName: {type: String, default: ''},
-  location: {type: String, default: ''},
-  bio: {type: String, default: ''},
-  bgType: {type: String, default: 'color'},
-  bgColor: {type: String, default: '#00cfbd'},
-  bgImage: {type: String, default: ''},
-  buttons: {type: Object, default: {}},
-  links: {type: Object, default: []},
-}, { 
-  timestamps: true      // Agrega automáticamente los campos 'createdAt' y 'updatedAt'
-});
+  {
+    timestamps: true, // Agrega automáticamente los campos 'createdAt' y 'updatedAt'
+  }
+);
 
 // Exportamos el modelo 'Page'
 // Si el modelo 'Page' ya existe en 'models', lo utilizamos; si no, lo creamos
